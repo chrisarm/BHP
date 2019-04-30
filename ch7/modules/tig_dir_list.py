6f8d8a8c907b393fc1669424d18f41eed21bd1a0
chrisarm <chrisarm@gmail.com>
2019-04-30 00:05:38 -0700

    Moving to new directory

--- ch7/modules/tig_dir_list.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-from os import listdir
-
-
-def run(**args):
-    print('[*] In dir_list module.')
-    files = listdir('.')
-    return str(files)
--- ch7/modules/tig_environment.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-
-def run(**args):
-    print('[*] In environment module')
-    return str(os.environ)
